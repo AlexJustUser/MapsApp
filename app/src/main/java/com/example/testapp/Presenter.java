be373eb0c4831d5d5ccc9e95356724f30fdd0359
Aleks <swimmersuper16@gmail.com>
2021-04-04 22:58:24 +0300

    Create mvp classes

---
+++ app/src/main/java/com/example/testapp/Presenter.java
@@ -0,0 +1,21 @@
+package com.example.testapp;
+
+public class Presenter {
+
+    interface EnterActivity{
+
+        void showProgress();
+
+        void hideProgress();
+
+        void setMessage(String string);
+
+    }
+
+    interface ListActivity{
+
+
+
+    }
+
+}
