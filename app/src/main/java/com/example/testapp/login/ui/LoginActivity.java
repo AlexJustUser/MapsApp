a5766e58fa848926231912ffe20552cfc206a241
Aleks <swimmersuper16@gmail.com>
2021-04-24 13:42:50 +0300

    Add animation of buttons

--- app/src/main/java/com/example/testapp/login/ui/LoginActivity.java
+++ app/src/main/java/com/example/testapp/login/ui/LoginActivity.java
@@ -2,8 +2,10 @@ package com.example.testapp.login.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 import com.example.testapp.R;
@@ -25,18 +27,31 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
         init();
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     public void init() {
         presenter = new LoginPresenter(this, this);
 
-        binding.loginButton.setOnClickListener(v -> {
-            if (binding.loginEdit.getText().toString().equals("") || binding.loginPasswordEdit.getText().toString().equals("")) {
-                updateUserInfoTextView(getResources().getString(R.string.insert));
-            } else {
-                setProgressBarVisible(true);
-                presenter.checkAccess(binding.loginEdit.getText().toString(), binding.loginPasswordEdit.getText().toString());
-                if(binding.loginCheckBox.isChecked()){
-                    //logic to save login and password if user wants it
+        binding.loginButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    binding.loginButton.setTextColor(getResources().getColor(R.color.colorBlue));
+                    binding.loginButton.setBackground(getResources().getDrawable(R.drawable.background_white_round_solid));
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (binding.loginEdit.getText().toString().equals("") || binding.loginPasswordEdit.getText().toString().equals("")) {
+                        updateUserInfoTextView(getResources().getString(R.string.insert));
+                    } else {
+                        setProgressBarVisible(true);
+                        presenter.checkAccess(binding.loginEdit.getText().toString(), binding.loginPasswordEdit.getText().toString());
+                        if(binding.loginCheckBox.isChecked()){
+                            //logic to save login and password if user wants it
+                        }
+                    }
+                    binding.loginButton.setTextColor(getResources().getColor(R.color.colorWhite));
+                    binding.loginButton.setBackground(getResources().getDrawable(R.drawable.background_white_round_corner));
                 }
+
+                return true;
             }
         });
     }
--- app/src/main/java/com/example/testapp/map/ui/MapActivity.java
+++ app/src/main/java/com/example/testapp/map/ui/MapActivity.java
@@ -1,13 +1,14 @@
 package com.example.testapp.map.ui;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.testapp.R;
 import com.example.testapp.databinding.ActivityMapBinding;
-import com.example.testapp.login.ui.LoginActivity;
-import com.example.testapp.mapslist.ui.MapsListActivity;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -19,6 +20,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private ActivityMapBinding binding;
     private Intent intent;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +30,20 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        binding.backwardButton.setOnClickListener(v -> {
-            finish();
+        binding.backwardButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    binding.backwardButton.setTextColor(getResources().getColor(R.color.colorBlue));
+                    binding.backwardButton.setBackground(getResources().getDrawable(R.drawable.background_white_round_solid));
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    binding.backwardButton.setTextColor(getResources().getColor(R.color.colorWhite));
+                    binding.backwardButton.setBackground(getResources().getDrawable(R.drawable.background_white_round_corner));
+                    finish();
+                }
+
+                return true;
+            }
         });
     }
 
